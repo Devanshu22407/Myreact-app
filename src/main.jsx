95c731596db1cdbdc3fb2a5d973e46702a1d7663
Devanshu22407 <devanshusheladiya407@gmail.com>
2025-07-31 11:58:47 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import EventDemo from './EventDemo.jsx'
 import Counter from './Counter.jsx'
+import DoSum from './DoSum.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
-  </StrictMode>,
+    <DoSum/>
+  </StrictMode>
 )
